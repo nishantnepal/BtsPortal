f7b84da8d8d4d4690e43bb817a42416d50511dc6
nishantnepal <nepalnishant@gmail.com>
2016-12-05 12:48:12 -0500

    exception cleanup

--- BtsPortal.Services.EsbAlert/AppSettings.cs
+++ BtsPortal.Services.EsbAlert/AppSettings.cs
@@ -7,7 +7,7 @@ namespace BtsPortal.Services.EsbAlert
         public static string EsbExceptionDbConnectionString
             => ConfigurationManager.ConnectionStrings["EsbExceptionDbConnection"].ConnectionString;
 
-        public const string SERVICE_NAME = "IA.ESB.Alert";
+        public const string SERVICE_NAME = "BtsPortal.Alert";
         public const int DEFAULT_INTERVAL = 60000;
         public const int DEFAULT_BATCH_SIZE = 500;
     }
